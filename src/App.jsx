32c3863fbc77f5d5dc2b1a0e119d98fedf6a14ca
Ktheoret <Kristin.Theoret@gmail.com>
2025-06-13 13:59:28 -0400

    Implemented a Footer component and imported it in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './Header.jsx'
+import Header from './Footer.jsx'
 
 function App() {
   
   return(
-      <Header/>
+      <>
+        <Header/>
+        <Footer/>
+      </>
   );
 }
 
